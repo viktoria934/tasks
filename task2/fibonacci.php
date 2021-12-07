f5cc234ec1133ed3ff18e91571dcca926969eddf
Viktoryia Barysenka <mariamovna@icloud.com>
2021-12-07 12:17:54 +0300

    task 2

--- task2/fibonacci.php
+++ task2/fibonacci.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 class Fibonacci
 {
@@ -55,3 +55,4 @@ class Fibonacci
 
 $fibonacci = new Fibonacci();
 echo $fibonacci->loopFibonacci('10000');
+
