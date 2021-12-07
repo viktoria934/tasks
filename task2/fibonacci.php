b2aba1b37d096dead45c9ab1a85c01ed22bc0950
Viktoryia Barysenka <mariamovna@icloud.com>
2021-12-07 14:11:05 +0300

    fix task

--- task2/fibonacci.php
+++ task2/fibonacci.php
@@ -3,12 +3,37 @@ declare(strict_types=1);
 
 class Fibonacci
 {
+    /**
+     * @param string $n
+     * @return string
+     */
+    public function loopFibonacci(string $n): string
+    {
+        $first = '0';
+        $second = '1';
+        for ($i = 1; $i < $n; $i++) {
+            $sum = $this->sum($first, $second);
+            $first = $second;
+            $second = $sum;
+            if (strlen($sum) >= 100) {
+                return $sum;
+            }
+        }
+        return $sum;
+    }
+
+    /**
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
     public function sum(string $first, string $second): string
     {
         $result = '';
         $ten = 0;
 
-        $first = $this->addNull($first, $second);
+
+        $first = $this->addZeros($first, $second);
 
         for ($i = strlen($second) - 1; $i >= 0; $i--) {
             $sum = (int)$first[$i] + (int)$second[$i];
@@ -28,31 +53,17 @@ class Fibonacci
         return $result;
     }
 
-    public function addNull(string $first, string $second): string
+    /**
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
+    public function addZeros(string $first, string $second): string
     {
-        if (strlen($first) <= strlen($second)) {
-            $result = strlen($second) - strlen($first);
-            $count = str_repeat('0', $result);
-            $first = $count . $first;
-        }
-        return $first;
-    }
-
-    public function loopFibonacci(string $n): string
-    {
-        $first = '0';
-        $second = '1';
-        for ($i = 1; $i < $n; $i++) {
-            $sum = $this->sum($first, $second);
-            $first = $second;
-            $second = $sum;
-            if (strlen($sum) >= 100) {
-                return $sum;
-            }
-        }
+        return str_pad($first, strlen($second), "0", STR_PAD_LEFT);
     }
 }
 
 $fibonacci = new Fibonacci();
-echo $fibonacci->loopFibonacci('10000');
+echo $fibonacci->loopFibonacci('8') . PHP_EOL;
 
--- task2/tests/fibonacciTest.php
+++ task2/tests/fibonacciTest.php
@@ -4,11 +4,54 @@ require_once __DIR__ . '/../fibonacci.php';
 
 class fibonacciTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_digit()
+    private Fibonacci $fibonacci;
+
+    public function setUp(): void
     {
-        $fibonacci = new Fibonacci();
-        $result = $fibonacci->loopFibonacci('10000');
+        $this->fibonacci = new Fibonacci();
+        parent::setUp();
+    }
+
+
+    public function test_loopFibonacci()
+    {
+        $result = $this->fibonacci->loopFibonacci('10000');
         $this->assertIsNumeric($result);
+    }
+
+    /**
+     * @dataProvider additionSecondProvider
+     */
+    public function test_addZeros($a, $b, $expected)
+    {
+        $this->assertSame((int)$expected, (int)$this->fibonacci->addZeros($a, $b), 'method addZeros is correct');
+    }
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function test_sum($a, $b, $expected)
+    {
+        $this->assertSame((int)$expected, (int)$this->fibonacci->sum($a, $b), 'method sum is correct');
+    }
+
+    public function additionProvider()
+    {
+        return [
+            ['0', '0', '0'],
+            ['0', '1', '1'],
+            ['1', '0', '1'],
+            ['1', '1', '2']
+        ];
+    }
+
+    public function additionSecondProvider()
+    {
+        return [
+            ['10', '1000', '0010'],
+            ['20', '1000', '0020'],
+            ['10', '10', '10'],
+        ];
     }
 }
 
